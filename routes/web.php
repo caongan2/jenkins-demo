444bde9d295ef9114ee9f314b7758d1fee057901
ngan_ct <caothuongngan@gmail.com>
2023-11-04 00:55:40 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('jenkins');
 });
 Route::get('/test-view', function () {
     return view('jenkins');
